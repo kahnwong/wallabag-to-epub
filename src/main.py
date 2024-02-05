3867b1526611c0de2e4d81fe8cba15b3281f13a2
Karn Wong <karn@karnwong.me>
2024-02-05 09:47:58 +0700

    refactor main.py

--- src/main.py
+++ src/main.py
@@ -1,35 +1,46 @@
 import json
+from typing import Any
 
 import xml2epub
 
-# read data
-filepath = "/Users/kahnwong/Downloads/Unread articles.json"
-with open(filepath, "r") as f:
-    data = json.load(f)
 
-# sort from oldest to newest
-data = data[::-1]
-data = data[:140]  # debug
+def parse_data(filename: str) -> Any:
+    with open(filename, "r") as f:
+        data = json.load(f)
 
-chunksize = 20
-data = [data[x : x + chunksize] for x in range(0, len(data), chunksize)]
+    # sort from oldest to newest
+    data = data[::-1]
+    data = data[:140]  # debug
 
-# main
-for index, chunk in enumerate(data):
-    book = xml2epub.Epub(f"chunk {index}")
+    chunksize = 20
+    data = [data[x : x + chunksize] for x in range(0, len(data), chunksize)]
 
-    for article in chunk:
-        title = article["title"]
-        content = f"<h1>{title}</h1>" + article["content"]
+    return data
 
-        print(f"Adding {title}...")
 
-        try:
-            book.add_chapter(
-                xml2epub.create_chapter_from_string(html_string=content, title=title)
-            )
-        except ValueError:
-            pass
+def create_epub(data: Any):
+    for index, chunk in enumerate(data):
+        book = xml2epub.Epub(f"chunk {index}")
 
-    ## generate epub file
-    book.create_epub("output")
+        for article in chunk:
+            title = article["title"]
+            content = f"<h1>{title}</h1>" + article["content"]
+
+            print(f"Adding {title}...")
+
+            try:
+                book.add_chapter(
+                    xml2epub.create_chapter_from_string(
+                        html_string=content, title=title
+                    )
+                )
+            except ValueError:
+                pass
+
+        ## generate epub file
+        book.create_epub("output")
+
+
+if __name__ == "__main__":
+    data = parse_data("/Users/kahnwong/Downloads/Unread articles.json")
+    create_epub(data)
