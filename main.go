0ac4cab740ed0a4d8f6a772f58917960ba074088
Karn Wong <karn@karnwong.me>
2024-08-03 17:42:34 +0700

    use chunks to create multiple epub files

--- main.go
+++ main.go
@@ -22,6 +22,28 @@ func getEntries(length int) []wallabago.Item {
 	return entries.Embedded.Items
 }
 
+func Chunk[T any](slice []T, n uint64) <-chan []T {
+	if n == 0 {
+		panic("n can`t be less than 1")
+	}
+
+	channel := make(chan []T, 1)
+
+	go func() {
+		defer close(channel)
+		for i := uint64(0); i < uint64(len(slice)); i += n {
+			// Clamp the last chunk to the slice bound as necessary.
+			end := min(n, uint64(len(slice[i:])))
+
+			// Set the capacity of each chunk so that appending to a chunk does
+			// not modify the original slice.
+			channel <- slice[i : i+end : i+end]
+		}
+	}()
+
+	return channel
+}
+
 func init() {
 	wallabagConfig := wallabago.WallabagConfig{
 		WallabagURL:  os.Getenv("WALLABAG_URL"),
@@ -38,38 +60,49 @@ func init() {
 }
 
 func main() {
+	var outputFileIndex int
+
 	// get entries
-	entries := getEntries(2) // debug
-	fmt.Println(len(entries))
+	entries := getEntries(200)
 
-	// Create a new EPUB
-	e, err := epub.NewEpub("My title")
-	if err != nil {
-		log.Println(err)
-	}
+	// create EPUBs
+	chunks := Chunk(entries, 20)
 
-	// Set the author
-	e.SetAuthor("Wallabag")
+	for chunk := range chunks {
+		filename := fmt.Sprintf("Wallabag %v.epub", outputFileIndex)
+		fmt.Printf("Creating file: %s\n", filename)
 
-	// add articles
-	for _, entry := range entries {
-		fmt.Println(entry.Title)
-
-		// Add a section
-		_, err = e.AddSection(entry.Content, entry.Title, "", "")
+		// Create a new EPUB
+		e, err := epub.NewEpub(filename)
 		if err != nil {
-			log.Println(err)
+			log.Println("Error initializing Epub:", err)
 		}
-	}
 
-	// write epub
-	fmt.Println("Embedding images...")
-	e.EmbedImages() // this has to stay here
+		// Set the author
+		e.SetAuthor("Wallabag")
 
-	err = e.Write("output/My EPUB.epub")
-	if err != nil {
-		fmt.Println("Error creating EPUB")
-	} else {
-		fmt.Println("Successfully created EPUB")
+		// Add articles
+		for _, entry := range chunk {
+			fmt.Printf("Adding: %s\n", entry.Title)
+
+			// Add section
+			_, err = e.AddSection(entry.Content, entry.Title, "", "")
+			if err != nil {
+				log.Println("Error adding article", err)
+			}
+		}
+
+		// write epub
+		fmt.Println("Embedding images...")
+		e.EmbedImages() // this has to stay here
+
+		err = e.Write(fmt.Sprintf("output/%s", filename))
+		if err != nil {
+			fmt.Println("Error creating EPUB")
+		} else {
+			fmt.Println("Successfully created EPUB")
+		}
+
+		outputFileIndex++
 	}
 }
